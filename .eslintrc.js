bd90ac3c96e1b9e15b0a8129750ab12dbe210fb8
xuwu <zx597813039@gmail.com>
2021-08-19 21:11:07 +0800

    feat: init project

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "root": true,
+  "env": {
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 2020
+  },
+  "rules": {
+    "no-console": "off",
+    "no-debugger": "off"
+  }
+}
